e2688f442e0a2ce8360a86ee6c8dae65b38f0a4b
shafaet.hossain@g.bracu.ac.bd <shafaet.hossain@g.bracu.ac.bd>
2024-04-05 05:04:49 +0600

    added login route

---
+++ src/pages/LogIn/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2 className="text-2xl">This is log in</h2>
+    </div>
+  );
+};
+
+export default Login;
